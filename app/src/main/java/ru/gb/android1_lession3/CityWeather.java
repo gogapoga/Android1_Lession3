377262f279e7ec0dd5b00737719558f4e8aeeb36
gogapoga <gogapoga1981@yandex,ru>
2019-01-17 19:16:14 +0300

    ДЗ к 4-му уроку

--- app/src/main/java/ru/gb/android1_lession3/CityWeather.java
+++ app/src/main/java/ru/gb/android1_lession3/CityWeather.java
@@ -2,18 +2,17 @@ package ru.gb.android1_lession3;
 
 import java.io.Serializable;
 
-public class CityWeather implements Serializable {
+public class CityWeather  implements Serializable {
 
     private String name;
     private int[] temp;
     private int[] humidity;
     private int[] pressure;
     private int[] wind;
-    private Boolean init;
+    private boolean init;
 
     public CityWeather(String name) {
         this.name = name;
-        init = false;
     }
     public void loadWeather() {  // загрузка погоды для города, пока случайным образом
         temp = new int[24];
@@ -22,7 +21,7 @@ public class CityWeather implements Serializable {
         wind = new int[24];
         for(int i = 0; i < 24; i++) {
             temp[i] = (int)(Math.random() * 10) - 5;
-            humidity[i] = (int)(Math.random() * 50) + 30;
+            humidity[i] = (int)(Math.random() * 80) + 20;
             pressure[i] = (int)(Math.random() * 50) + 725;
             wind[i] = (int)(Math.random() * 25) + 1;
         }
@@ -31,16 +30,16 @@ public class CityWeather implements Serializable {
     public String getName() {
         return name;
     }
-    public String getWind(int time) {
-        return init ? String.valueOf(wind[time]) + " м/с" : "нет данных";
+    public int getWind(int time) {
+        return wind[time];
     }
-    public String getHumidity(int time) {
-        return init ? String.valueOf(humidity[time]) + "%" : "нет данных";
+    public int getHumidity(int time) {
+        return humidity[time];
     }
-    public String getPressure(int time) {
-        return init ? String.valueOf(pressure[time]) + " мм.рт.ст." : "нет данных";
+    public int getPressure(int time) {
+        return pressure[time];
     }
-    public String getTemp(int time) {
-        return init ? String.valueOf(temp[time]) + " °C" : "нет данных";
+    public int getStrTemp(int time) {
+        return temp[time];
     }
 }
